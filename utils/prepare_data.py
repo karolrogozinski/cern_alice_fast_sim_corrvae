c71c4ab12801fa2239f19a956bb3049513781e58
Karol Rogoziński <73389492+karolrogozinski@users.noreply.github.com>
2023-11-17 18:26:31 +0100

    Added more properties

--- utils/prepare_data.py
+++ utils/prepare_data.py
@@ -5,19 +5,44 @@ from torch.utils.data import TensorDataset
 
 
 data = np.load('./data/data_nonrandom_responses.npz')["arr_0"]
-data = torch.Tensor(np.float32(np.log(data+1)))
+height, width = (data.shape[1], data.shape[2])
+ 
+max_data = np.max(data)
+max_sum = np.max([img.sum() for img in data])
 
+data = torch.Tensor(np.float32(np.log(data+1)))
 data_torch = torch.Tensor(data).unsqueeze(1)
 
-y_data = np.ones([1, 2])
+y_data = np.ones([1, 7])
 
+# All have to be in 0-1 range
 for img in data:
-    # TODO Normalise size
-    x = (img.argmax() % 44) / 44
-    y = (img.argmax() // 44) / 44
-    # size = np.count_nonzero(img)
-    # m = (img.max())
-    y_data = np.concatenate((y_data, np.asarray([[x, y]])), axis=0)
+    # max coords
+    x_max = (img.argmax() % height) / height
+    y_max = (img.argmax() // height) / height
+
+    # sum of pixels
+    weights_sum = img.sum()
+    var_sum = weights_sum / max_sum
+
+    # mass center
+    y_coords, x_coords = torch.meshgrid(
+        torch.arange(height), torch.arange(width))
+
+    x_sum = (x_coords * img).sum()
+    y_sum = (y_coords * img).sum()
+
+    x_mass = x_sum / weights_sum / 44
+    y_mass = y_sum / weights_sum / 44
+
+    # Size not 0
+    not_0 = np.count_nonzero(img) / (height * width)
+
+    # Max value
+    m = (img.max()) / max_data
+
+    y_data = np.concatenate((y_data, np.asarray(
+        [[x_max, y_max, x_mass, y_mass, not_0, var_sum, m]])), axis=0)
 
 y_data = np.float32(y_data[1:])
 y_torch = torch.tensor(y_data)
