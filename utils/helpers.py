70b04045557d48af03b287d2a0f2010b0d95e05b
Karol Rogoziński <73389492+karolrogozinski@users.noreply.github.com>
2023-12-15 00:45:44 +0100

    Adapted for cond data

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,44 +1,6 @@
-import numpy as np
-
 import torch
-from torch.utils.data import TensorDataset, DataLoader
-from torchvision import transforms
 
 import matplotlib.pyplot as plt
-
-
-def prepare_dataloader(data, batch_size):
-    data = np.float32(np.log(data+1))
-
-    max_data = np.max(data)
-
-    data = data / max_data
-
-    transform = transforms.Pad((10, 10))
-
-    data_torch = torch.Tensor(data)
-    data_torch = transform(data_torch).unsqueeze(1)
-
-    y_data = np.ones([1, 2])
-
-    for img in data:
-        # TODO Normalise size
-
-        x = (img.argmax() % 64) / 64
-        y = (img.argmax() // 64) / 64
-        # size = np.count_nonzero(img)
-        # m = (img.max())
-        y_data = np.concatenate((y_data, np.asarray([[x, y]])), axis=0)
-
-    y_data = y_data[1:]
-    y_data = np.float32(y_data)
-
-    y_torch = torch.tensor(y_data)
-
-    dataset = TensorDataset(data_torch, y_torch)
-    dataloader = DataLoader(dataset, shuffle=True, batch_size=batch_size)
-
-    return dataloader
 
 
 def plot_epoch(data_loader, model, device, taus, epoch, time):
@@ -46,9 +8,12 @@ def plot_epoch(data_loader, model, device, taus, epoch, time):
     print("Completed Epoch", epoch, " Time: ", time)
     print("===============================================================")
 
-    for data, _ in data_loader:
+    for (data, label) in data_loader:
         data = data.to(device)
-        (reconstruct, _), _, _, _, latent_sample_w, _, _ = model(data, taus)
+        cond = label[:, 9:].to(device)
+
+        (reconstruct, _), _, _, _, latent_sample_w, _, _, _ = model(
+            data, cond, taus)
 
         fig, axs = plt.subplots(3, 7, figsize=(15, 6))
         for i in range(21):
@@ -57,8 +22,9 @@ def plot_epoch(data_loader, model, device, taus, epoch, time):
             elif i < 14:
                 x = reconstruct.cpu().detach().numpy()[i-7][0]
             else:
-                (reconstruct_new_w, _), _, _, _, _, _, _ = model(
-                    data[-1], taus, w2=latent_sample_w[i-14])
+                (reconstruct_new_w, _), _, _, _, _, _, _, _ = model(
+                    data[-1], cond[i-14].unsqueeze(0), taus,
+                    w2=latent_sample_w[i-14])
                 x = reconstruct_new_w.cpu().detach().numpy()[0][0]
 
             im = axs[i//7, i % 7].imshow(x, interpolation='none',
