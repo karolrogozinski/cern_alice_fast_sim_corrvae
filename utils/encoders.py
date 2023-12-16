c2918c217d7fbcd63aeae3fb6b135838dd9a8ffa
Karol Rogoziński <73389492+karolrogozinski@users.noreply.github.com>
2023-12-17 00:41:32 +0100

    Reverse to cond

---
+++ utils/encoders.py
@@ -0,0 +1,95 @@
+"""
+Module containing the encoders.
+"""
+import numpy as np
+
+import torch
+from torch import nn
+
+
+class EncoderControlVAE(nn.Module):
+    def __init__(
+            self,
+            img_size,
+            latent_dim_z=10,
+            latent_dim_w=10,
+            latent_dim_cond=10,
+            hid_channels=32,
+            hidden_dim=512,
+            device='cpu',
+            ):
+        """
+        Encoder based on CorrVAE, adjusted to 44x44 ZDC images
+        """
+        super(EncoderControlVAE, self).__init__()
+
+        self.hid_channels = hid_channels
+        self.hidden_dim = hidden_dim
+        self.latent_dim_z = latent_dim_z
+        self.latent_dim_w = latent_dim_w
+        self.latent_dim_cond = latent_dim_cond
+        self.img_size = img_size
+
+        kernel_size = 4
+        cnn_kwargs = dict(
+            kernel_size=kernel_size,
+            stride=2,
+            padding=1,
+        )
+        self.reshape = (self.hid_channels, kernel_size, kernel_size)
+
+        self.conv1 = nn.Conv2d(
+            self.img_size[0], self.hid_channels, **cnn_kwargs).to(device)
+        self.conv2 = nn.Conv2d(
+            self.hid_channels, self.hid_channels, **cnn_kwargs).to(device)
+        self.conv3 = nn.Conv2d(
+            hid_channels, hid_channels, kernel_size,
+            stride=(3, 3), padding=(1, 1)).to(device)
+
+        self.lin1 = nn.Linear(
+            np.product(self.reshape) + self.latent_dim_cond,
+            self.hidden_dim).to(device)
+        self.lin2 = nn.Linear(self.hidden_dim, self.hidden_dim).to(device)
+
+        self.mu_logvar_gen = nn.Linear(
+            self.hidden_dim,
+            (self.latent_dim_z+self.latent_dim_w) * 2).to(device)
+
+        # self.lin1 = nn.Linear(
+        #     self.latent_dim_cond,
+        #     int(self.hidden_dim / 2)).to(device)
+        # self.lin2 = nn.Linear(int(self.hidden_dim / 2), self.hidden_dim).to(device)
+        # self.lin3 = nn.Linear(self.hidden_dim, self.hidden_dim * 2).to(device)
+
+        # Fully connected layers for mean and variance
+        self.mu_logvar_gen = nn.Linear(
+            self.hidden_dim,
+            (self.latent_dim_z+self.latent_dim_w) * 2).to(device)
+
+    def forward(self, x, cond):
+        batch_size = x.size(0)
+
+        x = torch.relu(self.conv1(x))
+        x = torch.relu(self.conv2(x))
+        x = torch.relu(self.conv3(x))
+
+        x_z = x.view((batch_size, -1))
+        x_z = torch.relu(self.lin1(torch.cat([x_z, cond], dim=-1)))
+        x_z = torch.relu(self.lin2(x_z))
+
+        # x_z = cond.view((batch_size, -1))
+
+        # x_z = torch.relu(self.lin1(x_z))
+        # x_z = torch.relu(self.lin2(x_z))
+        # x_z = torch.relu(self.lin3(x_z))
+
+        mu_logvar = self.mu_logvar_gen(x_z)
+        mu, logvar = mu_logvar.view(
+            -1, self.latent_dim_z+self.latent_dim_w, 2).unbind(-1)
+
+        return (
+            mu[:, :self.latent_dim_z],
+            mu[:, self.latent_dim_z:],
+            logvar[:, :self.latent_dim_z],
+            logvar[:, self.latent_dim_z:]
+        )
