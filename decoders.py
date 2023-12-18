60f4cc49a24c5539dcc7b5f11b1bfcb256e06655
Karol Rogoziński <73389492+karolrogozinski@users.noreply.github.com>
2023-12-18 13:34:05 +0100

    One more try with c-> [w. z]

--- decoders.py
+++ decoders.py
@@ -62,12 +62,12 @@ class DecoderControlVAE(nn.Module):
                 ).to(device)
             self.wp_lin_list.append(nn.Sequential(*layers))
 
-        self.lin1 = nn.Linear(
-            self.latent_dim_z + self.latent_dim_w + self.latent_dim_cond,
-            hidden_dim).to(self.device)
         # self.lin1 = nn.Linear(
-        #     self.latent_dim_z + self.latent_dim_w,
+        #     self.latent_dim_z + self.latent_dim_w + self.latent_dim_cond,
         #     hidden_dim).to(self.device)
+        self.lin1 = nn.Linear(
+            self.latent_dim_z + self.latent_dim_w,
+            hidden_dim).to(self.device)
         self.lin2 = nn.Linear(hidden_dim, hidden_dim).to(self.device)
         self.lin3 = nn.Linear(
             hidden_dim, np.product(self.reshape)).to(self.device)
@@ -103,8 +103,8 @@ class DecoderControlVAE(nn.Module):
 
     def forward(self, z, w, cond, w_mask):
         batch_size = z.size(0)
-        wzc = torch.cat([w, z, cond], dim=-1)
-        # wzc = torch.cat([w, z], dim=-1)
+        # wzc = torch.cat([w, z, cond], dim=-1)
+        wzc = torch.cat([w, z], dim=-1)
         prop = []
 
         wp = self.mask(w, w_mask)
